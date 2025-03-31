5e6750daee37fcb5e7fb2f329b79b464360ee755
iHelops <iHelops@yandex.ru>
2025-03-31 20:53:24 +0500

    Перенес токен в константу

--- src/constants/maptiler.ts
+++ src/constants/maptiler.ts
@@ -1,3 +1,3 @@
-export const mapTilerToken = import.meta.env.PUBLIC_MAPTILER_TOKEN || '';
+export const mapTilerToken = 'LD2jAiTY0DV6yIPr0L8q';
 
 export const mapTylerStyle = `https://api.maptiler.com/maps/0195ec46-8fcb-79c7-b42a-a788457436e2/style.json?key=${mapTilerToken}`
