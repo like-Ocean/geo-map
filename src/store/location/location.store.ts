0535d8cf19e0b1fb8ae7b0436a2568a78bd31ead
like-Ocean <my.program.is.false.2p1@gmail.com>
2025-04-18 01:59:17 +0500

    bug fixes

--- src/store/location/location.store.ts
+++ src/store/location/location.store.ts
@@ -4,32 +4,15 @@ import { create } from 'zustand/react';
 
 interface LocationState {
     location: OSMLocation | null;
-    cellSize: number;
-
-    hexagonValues: Record<string, number>;
-    selectedHexagonId: string | null;
 }
 
 interface LocationsActions {
     setLocation: (location: OSMLocation | null) => void;
-    setCellSize: (size: number) => void;
-
-    setHexagonValue: (hexId: string, value: number) => void;
-    selectHexagon: (hexId: string | null) => void;
 }
 
 export const useLocationsStoreBase = create<LocationState & LocationsActions>()((set) => ({
     location: null,
-    cellSize: 45,
     setLocation: (location) => set({ location }),
-    setCellSize: (cellSize) => set({ cellSize }),
-
-    hexagonValues: {},
-    selectedHexagonId: null,
-    setHexagonValue: (hexId, value) => set((state) => ({
-        hexagonValues: { ...state.hexagonValues, [hexId]: value }
-    })),
-    selectHexagon: (hexId) => set({ selectedHexagonId: hexId }),
 }));
 
 export const useLocationStore = createSelectors(useLocationsStoreBase);
